7d957d7c269a904bf2e6164608ac0aa23f9ad8ea
Raffael Sahli <sahli@gyselroth.com>
2017-06-23 13:23:35 +0200

    new test cases

--- tests/Http/RouterTest.php
+++ tests/Http/RouterTest.php
@@ -3,6 +3,7 @@ namespace Micro\Testsuite\Http;
 
 use \PHPUnit\Framework\TestCase;
 use \Micro\Http\Router;
+use \Micro\Http\Router\Route;
 use \Micro\Testsuite\MockLogger;
 
 class RouterTest extends TestCase
@@ -10,11 +11,53 @@ class RouterTest extends TestCase
     public function testInit()
     {
         $server = [
-            'PATH_INFO' => 'index.php/api/my/path'
+            'PATH_INFO' => 'index.php/api/my/path',
+            'REQUEST_METHOD' => 'PUT',
         ];
 
         $router = new Router($server, new MockLogger());
         $this->assertEquals($router->getPath(), 'index.php/api/my/path');
+        $this->assertEquals($router->getVerb(), 'put');
         return $router;
+    }
+
+    /**
+     * @depends testInit
+     */
+    public function testVerb($router)
+    {
+        $router->setVerb('GET');
+        $this->assertEquals($router->getVerb(), 'get');
+    }
+    
+
+    /**
+     * @depends testInit
+     */
+    public function testPath($router)
+    {
+        $router->setPath('index.php/api');
+        $this->assertEquals($router->getPath(), 'index.php/api');
+    }
+    
+
+    /**
+     * @depends testInit
+     */
+    public function testAddRoute($router)
+    {
+        $this->assertCount(0, $router->getRoutes());
+        $router->addRoute(new Route('/', 'Controller'));
+        $this->assertCount(1, $router->getRoutes());
+    }
+    
+
+    /**
+     * @depends testInit
+     */
+    public function testClearRoutes($router)
+    {
+        $router->clearRoutingTable();
+        $this->assertCount(0, $router->getRoutes());
     }
 }
