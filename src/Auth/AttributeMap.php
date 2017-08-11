ae29685e04cc39ff34f385c7c3baa651b2257dfe
Raffael Sahli <sahli@gyselroth.com>
2017-08-11 09:25:12 +0200

    changed author

--- src/Auth/AttributeMap.php
+++ src/Auth/AttributeMap.php
@@ -4,6 +4,7 @@ declare(strict_types = 1);
 /**
  * Micro
  *
+ * @author    Raffael Sahli <sahli@gyselroth.net>
  * @copyright Copyright (c) 2017 gyselroth GmbH (https://gyselroth.com)
  * @license   MIT https://opensource.org/licenses/MIT
  */
